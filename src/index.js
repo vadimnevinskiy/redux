42d2b98d8c8e6c0622721c7cd63712427eb3e741
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-02-20 18:21:19 +0500

    Added constants types.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './styles.css'
 // import {createStore} from "./createStore";
 import {createStore} from "redux";
 import {rootReducer} from "./redux/rootReducer";
+import {DECREMENT, INCREMENT} from "./redux/types";
 
 const counter = document.getElementById('counter')
 const addBtn = document.getElementById('add')
@@ -12,10 +13,10 @@ const themeBtn = document.getElementById('theme')
 const store = createStore(rootReducer, 0)
 
 addBtn.addEventListener('click', () => {
-    store.dispatch({type: 'INCREMENT'})
+    store.dispatch({type: INCREMENT})
 })
 subBtn.addEventListener('click', () => {
-    store.dispatch({type: 'DECREMENT'})
+    store.dispatch({type: DECREMENT})
 })
 asyncBtn.addEventListener('click', () => {
 
